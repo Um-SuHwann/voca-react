5f4e41fabdc6a09e0f24819de1e251683177fda4
엄수환 <umsuhwann132gmail.com>
2024-11-02 06:14:11 +0900

    CreateDay 완료

--- src/components/CreateDay.js
+++ src/components/CreateDay.js
@@ -1,15 +1,32 @@
+import { useEffect, useState } from "react";
+import useFetch from "../hooks/userFetch";
 export default function CreateDay() {
-  function onSubmit(e) {
-    e.preventDefault();
+  const initialDays = useFetch("http://localhost:3001/days");
+  const [days, setDays] = useState([]);
+  function addDay() {
+    const newDay = { day: days.length + 1, id: days.length + 1 };
+    fetch(`http://localhost:3001/days/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newDay),
+    }).then((res) => {
+      if (res.ok) {
+        alert("생성이 완료 되었씁니다!");
+        setDays([...days, newDay]);
+      }
+    });
   }
+  useEffect(() => {
+    if (initialDays) {
+      setDays(initialDays);
+    }
+  }, [initialDays]);
   return (
-    <form onSubmit={onSubmit}>
-      <div className="input_area">
-        <label>Eng</label>
-        <input type="text" placeholder="computer" />
-      </div>
-
-      <button>Day 추가</button>
-    </form>
+    <div>
+      <h3>현재 일수 : {days.length}일</h3>
+      <button onClick={addDay}>Day 추가</button>
+    </div>
   );
 }
